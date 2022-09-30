45a0c8eab911807ca3cbdffefbbca56574272a46
NiniDev <linantfin@gmail.com>
2022-09-30 21:09:20 +0200

    Generated Recipe-Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'recipe/:id',
+    loadChildren: () => import('./pages/recipe/recipe.module').then( m => m.RecipePageModule)
   }
 ];
 @NgModule({
