546c84fd22bf789d42dc5c51883049e8855732ab
NiniDev <linantfin@gmail.com>
2022-10-03 10:46:21 +0200

    Switched to observables

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Recipe } from '../models/recipe-model';
+import { Ingredient } from '../models/ingredient-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
   private store;
+  private _recipes: BehaviorSubject<Recipe[]> = new BehaviorSubject([]);
+  recipes: Observable<Recipe[]> = this._recipes.asObservable();
 
   constructor() {
     this.init();
@@ -15,60 +19,79 @@ export class RecipeService {
   async init() {
     this.store = new Storage();
     await this.store.create();
-    // this.store.set('recipes', []);
+    this.updateObservable();
   }
 
-  async getRecipes() {
-    const recipes = await this.store.get('recipes') || [];
+  getRecipes() {
+    return this.recipes;
+  }
+
+  private async updateObservable() {
+    const recipes = await this.store.get('recipes');
+    this._recipes.next(this.mappedRecipes(recipes) || []);
+  }
+
+  private mappedRecipes(recipes: any): Recipe[] {
     return recipes.map(recipe => {
-      return new Recipe(recipe.title, recipe.ingredients, recipe.instructions, recipe.durations, recipe.favorite, recipe.personCount, recipe.id);
+      return new Recipe(
+        recipe.title,
+        recipe.ingredients.map(ingredient => {
+          return new Ingredient(ingredient.name, ingredient.amount, ingredient.unit);
+        }),
+        recipe.instructions,
+        recipe.durations,
+        recipe.favorite,
+        recipe.personCount,
+        recipe.id
+      );
     });
   }
 
   async addRecipe(recipe: Recipe) {
-    const recipes = await this.getRecipes();
+    const recipes = this.mappedRecipes(await this.store.get('recipes') || []);
     recipes.push(recipe);
     await this.store.set('recipes', recipes);
+    this.updateObservable();
   }
 
   async removeRecipe(recipe: Recipe) {
-    const recipes = await this.getRecipes();
+    const recipes = this.mappedRecipes(await this.store.get('recipes') || []);
     const index = recipes.findIndex(item => item.getId() === recipe.getId());
     recipes.splice(index, 1);
     await this.store.set('recipes', recipes);
+    this.updateObservable();
   }
 
   async updateRecipe(recipe: Recipe, changes: { title?: string, ingredients?: any[], instructions?: object[], durations?: object[], favorite?: boolean, personCount?: number }) {
-    const recipes = await this.getRecipes() as Recipe[];
+    const recipes = this.mappedRecipes(await this.store.get('recipes') || []);
     const index = recipes.findIndex(item => item.getId() === recipe.getId());
     recipes[index].setTitle(changes.title || recipe.getTitle());
     recipes[index].setIngredients(changes.ingredients || recipe.getIngredients());
-    recipes[index].setInstructions(changes.instructions || recipe.getInstructions().map(instruction => instruction['text']));
+    recipes[index].setInstructions(changes.instructions || recipe.getInstructions());
     recipes[index].setDurations(changes.durations || recipe.getDurations());
     recipes[index].setFavorite(changes.favorite || recipe.getFavorite());
     recipes[index].setPersonCount(changes.personCount || recipe.getPersonCount());
     await this.store.set('recipes', recipes);
+    this.updateObservable();
   }
 
   async getRecipe(id: string) {
-    const recipes = await this.getRecipes();
-    console.log(recipes);
-    
+    const recipes = this.mappedRecipes(await this.store.get('recipes') || []);
     return recipes.find(recipe => recipe.getId() === id);
   }
 
   async getFavorites() {
-    const recipes = await this.getRecipes();
+    const recipes =this.mappedRecipes(await this.store.get('recipes') || []);
     return recipes.filter(recipe => recipe.getFavorite());
   }
 
   async getFavoritesCount() {
-    const recipes = await this.getRecipes();
+    const recipes = this.mappedRecipes(await this.store.get('recipes') || []);
     return recipes.filter(recipe => recipe.getFavorite()).length;
   }
 
   async getRecipeCount() {
-    const recipes = await this.getRecipes();
+    const recipes = this.mappedRecipes(await this.store.get('recipes') || []);
     return recipes.length;
   }
 }
--- src/app/services/shoppinglist.service.ts
+++ src/app/services/shoppinglist.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 import { ListItem } from '../models/listitem-model';
 
@@ -9,6 +10,8 @@ import { ListItem } from '../models/listitem-model';
 })
 export class ShoppinglistService {
   private store;
+  private _shoppingList: BehaviorSubject<ListItem[]> = new BehaviorSubject([]);
+  private shoppingList: Observable<ListItem[]> = this._shoppingList.asObservable();
 
   constructor() {
     this.init();
@@ -17,48 +20,64 @@ export class ShoppinglistService {
   async init() {
     this.store = new Storage();
     await this.store.create();
-    console.log('Storage initialized');
+    this.updateObservable();
+  }
+
+  getShoppingList(): Observable<ListItem[]> {
+    return this.shoppingList;
+  }
+
+  private async updateObservable() {
+    const shoppingList = await this.store.get('shoppingList');
+    this._shoppingList.next(this.mappedShoppinglist(shoppingList) || []);
+  }
+
+  private mappedShoppinglist(listItems: any): ListItem[] {
+    return listItems.map(listItem => {
+      return new ListItem(listItem.ingredient, listItem.checked, listItem.amount, listItem.id);
+    });
   }
 
   async deleteChecked() {
-    const shoppingList = await this.getShoppingList();
+    const shoppingList = this.mappedShoppinglist(await this.store.get('shoppingList') || []);
     const filteredList = shoppingList.filter(listItem => !listItem.getChecked());
     await this.store.set('shoppingList', filteredList);
-  }
-
-  async getShoppingList() {
-    const shoppingList = await this.store.get('shoppingList') || [];
-    return shoppingList.map(listItem => new ListItem(listItem.ingredient, listItem.checked, listItem.amount, listItem.id));
+    this.updateObservable();
   }
 
   async addListItem(listItem: ListItem) {
-    const shoppingList = await this.getShoppingList();
+    const shoppingList = this.mappedShoppinglist(await this.store.get('shoppingList') || []);
     shoppingList.push(listItem);
     await this.store.set('shoppingList', shoppingList);
+    this.updateObservable();
   }
 
   async removeListItem(listItem: ListItem) {
-    const shoppingList = await this.getShoppingList();
+    const shoppingList = this.mappedShoppinglist(await this.store.get('shoppingList') || []);
     const index = shoppingList.findIndex(item => item.getId() === listItem.getId());
     shoppingList.splice(index, 1);
     await this.store.set('shoppingList', shoppingList);
+    this.updateObservable();
   }
 
   async updateListItem(listItem: ListItem, changes: { amount?: number, checked?: boolean }) {
-    const shoppingList = await this.getShoppingList() as ListItem[];
+    const shoppingList = this.mappedShoppinglist(await this.store.get('shoppingList') || []);
     const index = shoppingList.findIndex(item => item.getId() === listItem.getId());
     shoppingList[index].setAmount(changes.amount || listItem.getAmount());
     shoppingList[index].setChecked(changes.checked || listItem.getChecked());
     await this.store.set('shoppingList', shoppingList);
+    this.updateObservable();
   }
 
   async clearShoppingList() {
     await this.store.remove('shoppingList');
+    this.updateObservable();
   }
 
   async addItems(items: ListItem[]) {
-    const shoppingList = await this.getShoppingList();
+    const shoppingList = this.mappedShoppinglist(await this.store.get('shoppingList') || []);
     shoppingList.push(...items);
     await this.store.set('shoppingList', shoppingList);
+    this.updateObservable();
   }
 }
